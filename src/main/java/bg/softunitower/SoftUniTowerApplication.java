c2deb9a608c389152b710a20a7d5f3c6fb9a6b8f
Vilimir Vladimirov <Viliom1@gmail.com>
2016-11-20 14:06:50 +0200

    First commit for DB repo

---
+++ src/main/java/bg/softunitower/SoftUniTowerApplication.java
@@ -0,0 +1,12 @@
+package bg.softunitower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftUniTowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftUniTowerApplication.class, args);
+	}
+}
