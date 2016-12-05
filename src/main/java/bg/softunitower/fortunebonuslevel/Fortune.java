03bff7a6154831fcc5f26febb10f0658cfa17655
MMQuants <mar4o86@mail.bg>
2016-12-05 14:21:33 +0200

    - magic numbers fixed

--- src/main/java/bg/softunitower/fortunebonuslevel/Fortune.java
+++ src/main/java/bg/softunitower/fortunebonuslevel/Fortune.java
@@ -4,20 +4,47 @@ package bg.softunitower.fortunebonuslevel;
 import javax.swing.*;
 
 public class Fortune {
+
     private LabyrinthEngine labyrinthEngine = new LabyrinthEngine();
+
+    private static final int SET_LOCATION_WIDTH = 640;
+
+    private static final int SET_LOCATION_HEIGHT = 477;
+
+    private static final int LABYRINTH_WIDHT = 680;
+
+    private static final int  LABYRINTH_HEIGHT = 720;
+
+    private static final int SET_LABYRINTH_X = 300;
+
+    private static final int SET_LABYRINTH_Y = 120;
+
 
     public Fortune() {
     }
 
     public void start() {
+
+
+
         this.labyrinthEngine.start();
+
         JFrame f = new JFrame();
-        f.setLocation(640, 477);
+
+        f.setLocation(SET_LOCATION_WIDTH, SET_LOCATION_HEIGHT);
+
         f.requestFocus();
-        this.labyrinthEngine.getLabyrinth().setBounds(0, 0, 680, 720);
+
+        this.labyrinthEngine.getLabyrinth().setBounds(0, 0, LABYRINTH_WIDHT, LABYRINTH_HEIGHT);
+
         f.add(this.labyrinthEngine.getLabyrinth());
+
         f.setTitle("Bonus Level: Fortune");
-        f.setBounds(300, 120, 680, 720);
+
+        f.setBounds(SET_LABYRINTH_X, SET_LABYRINTH_Y, LABYRINTH_WIDHT, LABYRINTH_HEIGHT);
+
         f.setVisible(true);
+
     }
+
 }
